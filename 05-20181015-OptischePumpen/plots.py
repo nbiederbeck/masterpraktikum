4a0488e530fc1b015abc3f7b8797c5e3ec6a35af
Maximilian Sackel <maximilian.sackel@gmx.de>
2018-10-15 18:32:35 +0200

    rename files

--- 05-20181015-OptischePumpen/plots.py
+++ 05-20181015-OptischePumpen/plots.py
@@ -3,31 +3,73 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy import signal
 
-df = pd.read_csv('data/TEK0007.CSV')
-data = pd.DataFrame(df.loc[18:].values[:,3:5],
-        columns=['time', 'voltage'])
+class larmor_sweep:
+    def __init__(self, csvPath, upperTLim=2.5e-3, lowerTLim=-1.5e-5):
+        df = pd.read_csv(csvPath)
+        data = pd.DataFrame(
+                df.loc[18:].values[:,3:5],
+                columns=['time', 'voltage'])
+        self.mask = (data.time > lowerTLim) & \
+                (data.time < upperTLim)
+        self.data = data[self.mask].reset_index()
+        self.lowerTLim = lowerTLim
+        self.upperTLim = upperTLim
 
-lowerTLim = -1e-5
-upperTLim =  2.5e-3
-mask = (data.time > lowerTLim) & (data.time < upperTLim)
-data = data[mask].reset_index()
+    def find_peaks(self, distance=None):
+        if distance==None:
+            distance=sum(self.mask)/20
+        self.uMean = np.mean(self.data['voltage'])
+        peaks, _ = signal.find_peaks(
+                self.data['voltage'], 
+                height=self.uMean, 
+                distance=sum(self.mask)/20)
+        self.tPeaks = self.data['time'][peaks]
+        self.uPeaks = self.data['voltage'][peaks]
+        return self.tPeaks, self.uPeaks
 
-uMean = np.mean(data['voltage'])
-peaks, _ = signal.find_peaks(
-        data['voltage'], 
-        height=uMean, 
-        distance=sum(mask)/20)
+    def save_info(self, path, find_peaks_dict={}):
+        self.find_peaks(*find_peaks_dict)
+        df = pd.DataFrame({
+            "lowerTLim": self.lowerTLim, 
+            "upperTLim": self.upperTLim,
+            "uMean": self.uMean,
+            "tPeaks": self.tPeaks,
+            "uPeaks": self.uPeaks,
+            "time": self.data.time,
+            "voltage": self.data.voltage
+            })
+        df.to_pickle(path)
 
-tPeaks = data['time'][peaks]
-uPeaks = data['voltage'][peaks]
+class plotter:
+    def __init__(self):
+        self.df = None
+        pass
 
-print('Peaks: ', tPeaks)
+    def load_data(self, path):
+        self.df = pd.read_pickle(path)
 
-fig = plt.figure()
-ax = fig.add_subplot(111)
-ax.plot(data['time'], data['voltage'])
-ax.scatter(tPeaks, uPeaks)
-ax.set_xlim(lowerTLim, upperTLim)
-ax.set_xlabel('Zeit / t')
-ax.set_ylabel('Spannung / U')
-fig.savefig('text.png')
+    def plot_exp(self, figPath):
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        ax.plot(1000*self.df.time, self.df.voltage)
+        ax.scatter(1000*self.df.tPeaks, 
+                self.df.uPeaks, 
+                label='detected peak')
+        ax.set_xlim(1000*self.df.lowerTLim[0], 
+                1000*self.df.upperTLim[0])
+        ax.set_xlabel('Zeit / ms')
+        ax.set_ylabel('Spannung / U')
+        plt.legend(loc='best')
+        fig.savefig(figPath)
+        plt.close()
+
+def main():
+    # lam = larmor_sweep('data/TEK0007.CSV')
+    # lam.save_info('test')
+
+    plttr = plotter()
+    plttr.load_data('test')
+    plttr.plot_exp('test.pdf')
+
+if __name__ == '__main__':
+    main()
