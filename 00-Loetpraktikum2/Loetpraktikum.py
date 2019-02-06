d7ebf2d517e717b616a49279009624de8d7afa11
Maximilian Sackel <maximilian.sackel@gmx.de>
2019-02-06 12:43:04 +0100

    add correction resistance

--- 00-Loetpraktikum2/Loetpraktikum.py
+++ 00-Loetpraktikum2/Loetpraktikum.py
@@ -1,14 +1,29 @@
 # coding: utf-8
 import pandas as pd
 from matplotlib import pyplot as plt
+from scipy.optimize import curve_fit
+import numpy as np
 
 spannungsteiler = pd.read_csv(
     'data/spannungsteiler.csv', sep='\s+', header=[0, 1])
 
+def f(x, a, b):
+    return a*x+b
+
 for header in spannungsteiler.columns.levels[0]:
     fig, ax = plt.subplots()
+   
+    current = spannungsteiler[header]['I/mA'].dropna()
+    voltage = spannungsteiler[header]['U/V'].dropna()
+
+    param, pcov = curve_fit(f, current, voltage)
+    print(param)
+    ax.plot(current, f(current, *param))
+
     spannungsteiler[header].plot.scatter(
         x='I/mA', y='U/V', label=header, ax=ax, figsize=(5.78, 3.57))
+    ax.set_title(r'Praktisch Ermittelter Widerstand ({:3.2f} +- {:3.2}) k$\Omega$'.format(
+        param[0], np.sqrt(np.diag(pcov))[0]))
     fig.tight_layout(pad=0)
     fig.savefig('plots/spannungsteiler_{}.pdf'.format(header))
 
